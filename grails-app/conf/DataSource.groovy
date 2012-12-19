9f2ebb5d03384d80f8be74241e9f59036bd56099
student <student@WebArch.localdomain>
2012-12-19 17:06:12 +0000

    found and fixed error in datasource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,7 +17,7 @@ environments {
             driverClassName = "com.mysql.jdbc.Driver"
             dialect=org.hibernate.dialect.MySQL5Dialect
             username = "webarch"
-            password = “webarch"
+            password = "webarch"
             url = "jdbc:mysql://localhost/eregister?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
             pooled = true
         }
